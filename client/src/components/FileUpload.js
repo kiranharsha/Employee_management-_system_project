17c7d81c9f054cc78daeb6b3cd4e906c9cc21972
kiranharsha <kiranharsha570@gmail.com>
2023-02-13 15:01:31 +0530

    progress bar and file upload status implementation

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,68 +1,94 @@
-import React,{useState} from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import Message from "./Message";
+import Progress from "./Progress";
+import axios from "axios";
 
 const FileUpload = () => {
-    const [file, setFile] = useState('');
-    const [fileNames, setFileName] = useState('Choose File');
-    const [uploadedFile, setUploadedFile] = useState({});
+  const [file, setFile] = useState("");
+  const [fileNames, setFileName] = useState("Choose File");
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState("");
+  const [uploadPercentage, setUploadPercentage] = useState(0);
 
-    const onChange = (e)=>{ 
-        // console.log(e.target.files)
-        setFile(e.target.files[0]);
-        setFileName(e.target.files[0].name);
+  const onChange = (e) => {
+    // console.log(e.target.files)
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(file)
+    // const formData = file
+    const formData = new FormData();
+    formData.append("file", file);
+    console.log(file);
+
+    try {
+      const res = await axios.post("http://localhost:5000/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        onUploadProgress: (ProgressEvent) => {
+          setUploadPercentage(
+            parseInt(
+              Math.round((ProgressEvent.loaded * 100) / ProgressEvent.total)
+            )
+          );
+
+          // clear percentage
+          setTimeout(() => setUploadPercentage(0), 10000);
+        },
+      });
+      console.log(res);
+      const { fileName, filePath } = res.data;
+      setUploadedFile({ fileName, filePath });
+
+      setMessage("File Uploaded");
+    } catch (err) {
+      if (err.response.status === 500) {
+        setMessage("There Was a Problem with Server");
+      } else {
+        setMessage(err.response.data.msg);
+      }
     }
-
-    const onSubmit = async e =>{
-        e.preventDefault();
-        // console.log(file)
-        // const formData = file
-        const formData = new FormData();
-        formData.append('file', file);
-        console.log(file)
-     
-
-        try{
-          const res = await axios.post('http://localhost:5000/upload', formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data'
-            },
-          });
-          console.log(res)
-          const {fileName, filePath} = res.data;
-          setUploadedFile({fileName, filePath});
-        }catch(err){
-          if(err.response.status === 500){
-            console.log('There Was a Problem with Server');
-          }else{
-            console.log(err.response.data.msg);
-          }
-        }
-    }
+  };
 
   return (
     <div>
-        <form onSubmit={onSubmit}>
+      {message ? <Message msg={message} /> : null}
+      <form onSubmit={onSubmit}>
         <div className="custom-file mb-4">
-        <input type="file" className="custom-file-input" id="customFile" onChange={onChange} />
-        <label className="custom-file-label" htmlFor="customFile">
-         {fileNames}
-        </label>
-      </div>
+          <input
+            type="file"
+            className="custom-file-input"
+            id="customFile"
+            onChange={onChange}
+          />
+          <label className="custom-file-label" htmlFor="customFile">
+            {fileNames}
+          </label>
+        </div>
 
-      <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4" />
-        </form>
+        <Progress percentage={uploadPercentage} />
 
-        { uploadedFile ? (
-        <div className= "row mt-5">
-          <div className='col-md-6 m-auto'>
-            <h3 className='text-center'>{uploadedFile.fileName}</h3>
-            <img style = {{width:'100%'}}src={uploadedFile.filePath} alt='' />
+        <input
+          type="submit"
+          value="Upload"
+          className="btn btn-primary btn-block mt-4"
+        />
+      </form>
+
+      {uploadedFile ? (
+        <div className="row mt-5">
+          <div className="col-md-6 m-auto">
+            <h3 className="text-center">{uploadedFile.fileName}</h3>
+            <img style={{ width: "100%" }} src={uploadedFile.filePath} alt="" />
           </div>
         </div>
-        ) : null}
-      
+      ) : null}
     </div>
   );
-}
+};
 
-export default FileUpload
+export default FileUpload;
